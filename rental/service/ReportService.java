70da5156b037086f54a7f39e92a34dc004aa1a44
Devansh020105 <157352794+Devansh020105@users.noreply.github.com>
2025-11-23 15:07:45 +0530

    Added ReportService.java

---
+++ rental/service/ReportService.java
@@ -0,0 +1,25 @@
+package rental.service;
+
+import rental.model.Vehicle;
+import java.util.*;
+
+public class ReportService {
+
+    public void generateReport(List<Vehicle> list) {
+        long available = list.stream().filter(Vehicle::isAvailable).count();
+        long rented = list.size() - available;
+
+        System.out.println("\n--- RENTAL REPORT ---");
+        System.out.println("Total Vehicles: " + list.size());
+        System.out.println("Available: " + available);
+        System.out.println("Rented: " + rented);
+        
+        System.out.println("\n--- RENTED VEHICLES ---");
+        for (Vehicle v : list) {
+            if (!v.isAvailable()) {
+                System.out.println(v.getVehicleId() + " (" + v.getModel() + ") - Rented by: " + v.getCustomerId());
+            }
+        }
+        System.out.println("---------------------");
+    }
+}
